f13c7b7e2efaa383dc87c0c519f04f000fd25dea
Jean-Michel Bodart <jbodart@argenx.com>
2024-10-01 23:11:02 +0200

    Update rad_sas.test.js
    Add examples filtering on list of rows or rows and columns indices

--- test/read_sas.test.js
+++ test/read_sas.test.js
@@ -1,25 +1,36 @@
-const { initWebR, read_sas, read_xpt } = require('../src/read_sas');
+const { initWebR, read_sas, read_xpt, read_sas_size, read_xpt_size } = require('../src/read_sas');
 const path = require('path');
 
 
 
 initWebR()
-   .then(() => {
-      return read_sas(path.join(__dirname, "dm.sas7bdat"));
+   .then(async () => read_sas_size(path.join(__dirname, "dm.sas7bdat")))
+   .then((size) => {
+      console.log('dm.sas7bdat, size:', size);
    })    
+   .then(async () => {console.log('dm first 8 rows, 2cols:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:8', '1:2'));})    
+   .then(async () => {console.log('5 rows, 3 cols:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:5', '1:3'));})    
+   .then(async () => {console.log('5 rows, 1  col:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:5', 1));})    
+   .then(() => read_sas(path.join(__dirname, "dm.sas7bdat")))   
    .then(data => { 
       console.log("\n\n=== dm.sas7bdat ===\n");
-      console.log(data.filter((d, i) => i < 5).map(d => { 
+      console.log('3 rows, selected cols:', data.filter((d, i) => i < 3).map(d => { 
          return ({STUDYID: d.STUDYID, USUBJID: d.USUBJID, RFSTDTC: d.RFSTDTC, RFENDTC: d.RFENDTC, AGE: d.AGE, SEX: d.SEX, RACE: d.RACE });
       })) 
    })
+   .then(async () => read_xpt_size(path.join(__dirname, "ds.xpt")))
+   .then((size) => {
+      console.log('ds.xpt, size:', size);
+   })       
    .then(() => {
       return read_xpt(path.join(__dirname, "ds.xpt"));
    })
    .then(data => {
       console.log("\n\n=== ds.xpt ===\n");
-      console.log(data.filter((d, i) => i < 5));
+      console.log('10 rows:', data.filter((v, i) => i < 10));
    })
+   .then(async () => console.log('5 rows:', await read_xpt(path.join(__dirname, "ds.xpt"), '1:5')))
+   .then(async () => console.log('5 rows, 3 col:', await read_xpt(path.join(__dirname, "ds.xpt"), '1:5', '1:3')))
    .catch(err => console.log(err))
 
 
