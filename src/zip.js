0b4e299899912eba25e13f1395c7d9d6148fc82f
Jean-Michel Bodart <jbodart@argenx.com>
2024-10-30 19:32:12 +0100

    Update zip.js
    Rewrite function extractZip() to use vscode Uris and vscode.workspace.fs

--- src/zip.js
+++ src/zip.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const Zip = require("adm-zip");
-const OriginalFs = require("original-fs");
+// const OriginalFs = require("original-fs");
+// const { validateVSCodeTypesCompatibility } = require("vsce/out/validation");
+const vscode = require('vscode');
 
 // Function to create a zip file and add files/folders to a subdirectory in the zip
 function createZip(zipFilePath /*: string*/, filesAndFolders /*: string[]*/, subdirectory = '' /*: string*/) /*: Promise<void>*/ {
@@ -39,17 +41,47 @@ function createZip(zipFilePath /*: string*/, filesAndFolders /*: string[]*/, sub
 }
 
 // Function to extract a zip file to a specified folder
-function extractZip(zipFilePath /*: string*/, extractToPath /*: string*/, overwrite = true) /*: Promise<void>*/ {
-   return new Promise((resolve, reject) => {
-      try {
-         // const zip = new Zip(zipFilePath);
-         const zip = new Zip(zipFilePath, {fs: OriginalFs});
-         zip.extractAllTo(extractToPath, overwrite); 
-         resolve();
-      } catch (err) {
-         reject(err);
+async function extractZip(zipFilePath, extractToPath, overwrite = true) {
+   try {
+      if (!(extractToPath instanceof vscode.Uri)) {
+         extractToPath = vscode.Uri.parse(extractToPath);
       }
-   });
+
+      let zip;
+      if (zipFilePath instanceof vscode.Uri) {
+         const zipContents = await vscode.workspace.fs.readFile(zipFilePath);
+         zip = new Zip(zipContents);
+      } else {
+         zip = new Zip(zipFilePath);
+      }
+
+      const zipEntries = zip.getEntries();
+      for (const zipEntry of zipEntries) {
+         const entryPath = vscode.Uri.joinPath(extractToPath, zipEntry.entryName);
+         let entryPathExists;
+         try {
+            await vscode.workspace.fs.stat(entryPath);
+            entryPathExists = true;
+         } catch (error) {
+            if (error.code === 'FileNotFound') {
+               entryPathExists = false;
+            } else {
+               throw error; // Some (other) error occurred
+            }
+         }
+         if (overwrite || !entryPathExists) {
+            if (zipEntry.isDirectory) {
+               await vscode.workspace.fs.createDirectory(entryPath);
+            } else {
+               await vscode.workspace.fs.writeFile(entryPath, zipEntry.getData());
+            }
+         } else {
+            console.log(`(extractZip) Skipped extracting zip entry as target exists: ${entryPath}`);
+         }
+      }
+   } catch (err) {
+      throw new Error(`Failed to extract zip: ${err.message}`);
+   }
 }
 
 module.exports = { createZip, extractZip };
