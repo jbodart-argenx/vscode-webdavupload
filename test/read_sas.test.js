77c1a73318db76ecdd019466954259aba4168c94
Jean-Michel Bodart <jmbodart@skynet.be>
2024-11-03 22:44:12 +0100

    Update read_sas.test.js
    Call webR.close() in catch()
    De-structure returned value
    Test row filtering by condition
    Test columns selection

--- test/read_sas.test.js
+++ test/read_sas.test.js
@@ -1,4 +1,4 @@
-const { initWebR, read_sas, read_xpt, read_sas_size, read_xpt_size } = require('../src/read_sas');
+const { initWebR, read_sas, read_xpt, read_sas_size, read_xpt_size } = require('../src/read_dataset');
 const path = require('path');
 
 let webR;
@@ -10,10 +10,11 @@ initWebR()
       console.log('dm.sas7bdat, size:', size);
    })    
    .then(async () => {console.log('dm first 8 rows, 2cols:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:8', '1:2'));})    
+   .then(async () => {console.log('dm first 8 rows, 2cols:', await read_sas(path.join(__dirname, "dm.sas7bdat"), `AGE <= 60 & SEX == 'M'`, '.(USUBJID, AGE, SEX)'));})    
    .then(async () => {console.log('5 rows, 3 cols:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:5', '1:3'));})    
    .then(async () => {console.log('5 rows, 1  col:', await read_sas(path.join(__dirname, "dm.sas7bdat"), '1:5', 1));})    
    .then(() => read_sas(path.join(__dirname, "dm.sas7bdat")))   
-   .then(data => { 
+   .then(({data}) => { 
       console.log("\n\n=== dm.sas7bdat ===\n");
       console.log('3 rows, selected cols:', data.filter((d, i) => i < 3).map(d => { 
          return ({STUDYID: d.STUDYID, USUBJID: d.USUBJID, RFSTDTC: d.RFSTDTC, RFENDTC: d.RFENDTC, AGE: d.AGE, SEX: d.SEX, RACE: d.RACE });
@@ -26,14 +27,17 @@ initWebR()
    .then(() => {
       return read_xpt(path.join(__dirname, "ds.xpt"));
    })
-   .then(data => {
+   .then(({data}) => {
       console.log("\n\n=== ds.xpt ===\n");
       console.log('10 rows:', data.filter((v, i) => i < 10));
    })
    .then(async () => console.log('5 rows:', await read_xpt(path.join(__dirname, "ds.xpt"), '1:5')))
-   .then(async () => console.log('5 rows, 3 col:', await read_xpt(path.join(__dirname, "ds.xpt"), '1:5', '1:3')))
+   .then(async () => console.log('5 rows, 3 col:', (await read_xpt(path.join(__dirname, "ds.xpt"), '1:5', '1:3'))))
    .then(async () => { webR.close() })
-   .catch(err => console.log(err))
+   .catch(err => {
+      console.log(err);
+      webR.close();
+   })
 
 
 /*
