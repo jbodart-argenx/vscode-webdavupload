d007ea307d5ef70afcc85c1796445f38334f2988
Jean-Michel Bodart <jbodart@argenx.com>
2024-11-13 10:25:57 +0100

    Update webpack.config.js
    Add react-big-table/build

--- webpack.config.js
+++ webpack.config.js
@@ -3,73 +3,68 @@
 
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-// import path from 'path';
-// import CopyWebpackPlugin from 'copy-webpack-plugin';
-
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);  // create a custom __dirname (not defined in ES module context)
+const { glob } = require('glob');
 
 /** @type {import('webpack').Configuration} */
 const config = {
-target: 'node', // Extensions run in a Node.js context
+    target: 'node', // Extensions run in a Node.js context
 
-entry: {
-   extension: './src/extension.js', // Entry point for the extension
-},
-output: {
-   filename: '[name].js',
-   path: path.resolve(__dirname, 'dist'),
-   libraryTarget: 'commonjs2',
-},
-mode: 'production',
-devtool: 'source-map',
-externals: {
-   vscode: 'commonjs vscode', // Exclude the vscode module
-},
-resolve: {
-   extensions: ['.js'],
-},
-module: {
-   rules: [
-      {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: 'babel-loader', // Use babel-loader for JavaScript files
-      },
-   ],
-},
-plugins: [
-   new CopyWebpackPlugin({
-      patterns: [
-      {
-         context: 'node_modules/webr/dist',
-         from: 'webr-*.js',
-      },
-      {
-         context: 'node_modules/webr/dist',
-         from: 'R.bin.*',
-      },
-      {
-         context: 'node_modules/webr/dist',
-         from: '*.so',
-      },
-      {
-         context: 'node_modules/webr/dist',
-         from: 'vfs',
-         to: 'vfs',
-      },
-      { 
-         from: 'webr-repo',
-         to: 'webr-repo'
-      }
-      ],
-   }),
-],
+    entry: {
+        extension: './src/extension.js', // Entry point for the extension
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'commonjs2',
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode', // Exclude the vscode module
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader', // Use babel-loader for JavaScript files
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    context: path.resolve(__dirname, 'node_modules/webr/dist'),
+                    from: 'webr-*.js',
+                },
+                {
+                    context: path.resolve(__dirname, 'node_modules/webr/dist'),
+                    from: 'R.bin.*',
+                },
+                {
+                    context: path.resolve(__dirname, 'node_modules/webr/dist'),
+                    from: '*.so',
+                },
+                {
+                    context: path.resolve(__dirname, 'node_modules/webr/dist'),
+                    from: 'vfs',
+                    to: 'vfs',
+                },
+                { 
+                    from: path.resolve(__dirname, 'webr-repo'),
+                    to: 'webr-repo'
+                },
+                {
+                    from: 'react-big-table/build',
+                    to: 'react-big-table/build'
+                }
+            ],
+        }),
+    ],
 };
 
 module.exports = config;
-// export default config;
