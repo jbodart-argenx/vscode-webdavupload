d99d819a6d8cd79c2976379b2115904116382743
Jean-Michel Bodart <jbodart@argenx.com>
2025-01-18 18:04:32 +0100

    Update eslint.config.mjs
    Fix config issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,7 @@
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginNode from "eslint-plugin-node";
+import eslintPluginPromise from "eslint-plugin-promise";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import globals from "globals";
 
 export default [
@@ -15,15 +19,31 @@ export default [
         },
         ignores: [
             "media/tabulator.min.js",
+            "media/script.js",
             "src/jobSubmission.js",
-            ".vscode-test/**"
+            "dist/**/*.js",
+            "node_modules/**/*.js",
+            ".vscode-test/**/*.js"
         ],
+        plugins: {
+            node: eslintPluginNode,
+            import: eslintPluginImport,
+            promise: eslintPluginPromise,
+            unicorn: eslintPluginUnicorn,
+        },
         rules: {
+            // Enable essential rules
+            //"node/no-unsupported-features/node-builtins": "off", // "error", // 
+            //"@typescript-eslint/no-explicit-any": "off", // Disable rule even if plugin is not used
+            //"@typescript-eslint/no-unused-vars": "off",
+            "import/no-extraneous-dependencies": "error",
+            "no-undef": "error", // Catch undefined variables
+            "eqeqeq": ["error", "always", { null: "ignore" }], // Require strict equality except in specific cases
             "no-const-assign": "warn",
             "no-this-before-super": "warn",
             "no-undef": "warn",
             "no-unreachable": "warn",
-            "no-unused-vars": "warn",
+            "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // Ignore unused variables starting with '_'
             "constructor-super": "warn",
             "valid-typeof": "warn",
         },
